e213e23a8f02fa1d2b5829d32d3f09bf420df4d8
venelouis <venelouistyago@gmail.com>
2026-06-11 22:12:14 -0300

    feat: update base path and add Dev.to and Hashnode links to footer

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Rullst/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
